c7ec6ee75459c614bf6bcfa4c23e6ad7da8863e2
DESKTOP-16C8CU5\bjoer <langb4@bfh.ch>
2024-05-31 08:48:18 +0200

    content to tooltip

--- list.js
+++ list.js
@@ -1,64 +1,69 @@
 export const courses = [
   {
-    title: "WDDA - Datenmanagement & Datenanalyse",
+    title: "WDDA - Data Management & Data Analysis",
     link: "https://moodle.bfh.ch/course/view.php?id=25863",
     summary:
-      "Kerninhalte: \n - SQL Grundlagen \n - Datenbankmanagment (DMBS) \n - Datenmodelle \n - Datenanalyse mit R \n - Statistik Grundlagen",
+      "Core contents:\n- SQL Basics\n- Database Management (DBMS)\n- Data Models\n- Data Analysis with R\n- Basics of Statistics",
   },
   {
     title: "WSEG - Software Engineering",
     link: "https://moodle.bfh.ch/course/view.php?id=29357",
-    summary: "Introduction to software engineering principles and practices.",
+    summary:
+      "Core contents:\n- Web Technologies\n- Conducting Software Projects\n- Angular\n- JavaScript \n- Git ",
   },
   {
-    title: "WSEN - Software-Entwicklung",
+    title: "WSEN - Software Development",
     link: "https://moodle.bfh.ch/course/view.php?id=24073",
-    summary: "Focuses on software development methodologies.",
+    summary:
+      "Core contents:\n- Python\n- Basics of Programming\n- Object Oriented Programming\n- Logic\n- Sets\n- Relations",
   },
   {
-    title: "WGWI - Grundlagen der Wirtschaftsinformatik",
+    title: "WGWI - Fundamentals of Business Informatics",
     link: "https://moodle.bfh.ch/course/view.php?id=24071",
-    summary: "Basic principles of business informatics.",
+    summary:
+      "Core contents:\n- Digitalization\n- Business Informatics as a Profession\n- Digital Infrastructures\n- Digital Business Models\n- Data Protection",
   },
   {
-    title: "WENG - Cross cultural Training",
+    title: "WENG - Cross Cultural Training",
     link: "https://moodle.bfh.ch/course/view.php?id=25956",
-    summary: "Training on cross-cultural communication and practices.",
+    summary:
+      "Core contents:\n- Intercultural Communication\n- Culture Frameworks\n- Hofstede\n- Business English ",
   },
   {
     title: "WBIS - Business Information Systems",
     link: "https://moodle.bfh.ch/course/view.php?id=25860",
-    summary: "Introduction to business information systems.",
+    summary:
+      "Core contents:\n- Business Uses for IT\n- Modern Business Ecosystems\n- Big Data\n- Social Media\n- Human Computer Interaction ",
   },
   {
-    title: "WPR1 - Projektmanagement",
+    title: "WPR1 - Project Management",
     link: "https://moodle.bfh.ch/course/view.php?id=28379",
-    summary: "Project management fundamentals and techniques.",
+    summary:
+      "Core contents:\n- Project Planning and Execution\n- Agility\n- Classic PM\n- Risk Analysis\n- Stakeholder Analysis",
   },
   {
     title: "EWEB - Web Engineering",
     link: "https://moodle.bfh.ch/course/view.php?id=31839",
-    summary: "Comprehensive overview of web engineering practices.",
+    summary:
+      "Core contents:\n- HTML\n- CSS\n- JavaScript\n- Web Frameworks\n- SEO & SEA\n- Online Marketing",
   },
   {
     title: "SBD2 - Data-Driven Visualization and Decision Making",
     link: "https://moodle.bfh.ch/course/view.php?id=31841",
-    summary: "Techniques for data visualization and informed decision making.",
+    summary:
+      "Core contents:\n- Big Data Analysis\n- Data Science Introduction\n- Data-Driven Visualization in R",
   },
   {
     title: "EAAI - Applied Artificial Intelligence",
     link: "https://moodle.bfh.ch/course/view.php?id=34801",
-    summary: "Applications of artificial intelligence in various fields.",
+    summary:
+      "Core contents:\n- AI Models\n- Computer Vision\n- Natural Language Processing (NLP)\n- AI Ethics",
   },
   {
     title: "EOSS - Open Source Software Management",
     link: "https://moodle.bfh.ch/course/view.php?id=34803",
-    summary: "Managing open-source software projects.",
-  },
-  {
-    title: "EENA - Enterprise Architecture",
-    link: "https://moodle.bfh.ch/course/view.php?id=36477",
-    summary: "Enterprice Architecture",
+    summary:
+      "Core contents:\n- OSPO 101\n- OS Initialization\n- Community Building\n- Github\n- OS Strategies",
   },
 ];
 
@@ -66,40 +71,47 @@ export const major = [
   {
     title: "SDA1 - Digital Design Architecture",
     link: "https://moodle.bfh.ch/course/view.php?id=33053",
-    summary: "Basics of digital design and architecture.",
+    summary:
+      "Core contents:\n- Enterprise Architecture (EA) Modeling\n- EA Strategy\n- Developing Architectures\n- TOGAF",
   },
   {
-    title: "SDA2 - Software Architectur",
+    title: "SDA2 - Software Architecture",
     link: "https://moodle.bfh.ch/course/view.php?id=31842",
-    summary: "Advanced concepts in software architecture.",
+    summary:
+      "Core contents:\n- UML\n- Software Architecture Concepts\n- Domain Driven Design\n- Layered Architecture\n- Micro Services",
   },
   {
     title: "SDA3 - UX & UI Design",
     link: "https://moodle.bfh.ch/course/view.php?id=33054",
-    summary: "Introduction to user experience and interface design.",
+    summary:
+      "Core contents:\n- User Interface\n- User Experience\n- Human Factors in UX\n- Figma\n- User Tests",
   },
 ];
 
 export const literature = [
   {
-    title: "Einstieg in Python - Thomas Theis",
+    title: "Introduction to Python - Thomas Theis",
     link: "https://www.rheinwerk-verlag.de/einstieg-in-python-ideal-fuer-programmiereinsteiger/",
-    summary: "A beginner's guide to Python programming.",
+    summary:
+      "This book guides step by step through basic concepts like variables, loops, and functions and offers insights into more advanced topics like object orientation and module usage.",
   },
   {
-    title: "Einführung in die Wirtschaftsinformatik - Jan Marco Leimeister",
+    title: "Introduction to Business Informatics - Jan Marco Leimeister",
     link: "https://link.springer.com/book/10.1007/978-3-540-77847-9",
-    summary: "Introduction to business informatics.",
+    summary:
+      "This book provides a sound and practice-oriented introduction to the entire field of business informatics. The main focuses include system platforms (hardware, operating systems), communication systems, databases, system development, IT application systems, IT management, and digital business.",
   },
   {
-    title: "Relationale Datenbanken - Thomas Studer",
+    title: "Relational Databases - Thomas Studer",
     link: "https://link.springer.com/book/10.1007/978-3-662-58976-2",
-    summary: "Comprehensive guide on relational databases.",
+    summary:
+      "This book by Thomas Studer offers a comprehensive introduction to relational databases, both theoretically and practically, with a focus on PostgreSQL. The target audience includes computer science and business informatics students as well as software developers.",
   },
   {
     title: "Eloquent JavaScript",
     link: "https://eloquentjavascript.net/",
-    summary: "A modern introduction to JavaScript programming.",
+    summary:
+      "Online book providing a comprehensive introduction to JavaScript. It covers both basic and advanced concepts and includes practical exercises and examples to deepen understanding.",
   },
 ];
 
@@ -107,129 +119,132 @@ export const repositories = [
   {
     title: "EWEB - Portfolio",
     link: "https://github.com/LANGB4/EWEB-Porfolio",
-    summary: "A portfolio of web engineering projects.",
+    summary: "This portfolio was created in the course EWEB - Web Engineering.",
   },
   {
     title: "CASE - Discover Bern API (SQL)",
     link: "https://github.com/LANGB4/DiscoAPI",
-    summary: "API for discovering Bern, implemented in SQL.",
+    summary: "API solution for the Discover Bern Project (Module EPRP-CASE)",
   },
   {
     title: "CASE - Discover Bern DB (MongoDB)",
     link: "https://github.com/LANGB4/DiscoverBernDB",
-    summary: "Database for Discover Bern project, using MongoDB.",
+    summary: "Database Prototype for CASE/EPRP ",
   },
   {
     title: "ERAP - hackday-automatisierung-berichte",
     link: "https://github.com/gilluuu/hackday-automatisierung-berichte",
-    summary: "Automated report generation for hackday projects.",
+    summary:
+      "Automated report generation Data Hackdays 2023 (ERAP - Hackathon and Rapid Prototyping)",
   },
   {
     title: "EWEB - Discover Bern (Angular)",
     link: "https://gitlab.com/d6334/discover-bern",
-    summary: "Angular application for discovering Bern.",
+    summary:
+      "Angular application for DiscoverBern (WSEG - Software Engineering)",
   },
 ];
 
 export const others = [
   {
-    title: "Data analytics (R basics)",
+    title: "Data Analytics (R Basics)",
     link: "https://moodle.bfh.ch/pluginfile.php/1898697/mod_resource/content/3/WDDA_HandoutsO_01.pdf",
-    summary: "Basic data analytics concepts using R.",
+    summary: "Slides: Basic data analytics concepts using R.",
   },
   {
-    title: "Business Object Modelling (SQL Basics)",
+    title: "Business Object Modeling (SQL Basics)",
     link: "https://moodle.bfh.ch/pluginfile.php/1893672/mod_resource/content/0/DB02_BusinessObjectModelling%20-%20vollst%C3%A4ndig.pdf",
-    summary: "Introduction to business object modeling with SQL.",
+    summary: "Slides: Introduction to business object modeling with SQL.",
   },
   {
-    title: "ER-Modelling (SQL Basics)",
+    title: "ER Modeling (SQL Basics)",
     link: "https://moodle.bfh.ch/pluginfile.php/1862898/mod_resource/content/2/DB03_ER-Modellierung1b.pdf",
-    summary: "Entity-relationship modeling basics.",
+    summary: "Slides: Entity-relationship modeling basics.",
   },
   {
     title: "WSEG Tools",
     link: "https://moodle.bfh.ch/mod/page/view.php?id=1600182",
-    summary: "Tools and resources for software engineering.",
+    summary: "List: Tools and resources for software engineering.",
   },
   {
     title: "Advanced Git Skills",
     link: "https://moodle.bfh.ch/pluginfile.php/2282617/mod_resource/content/1/Advanced%20Git%20Skills.pdf",
-    summary: "Guide to advanced Git techniques.",
+    summary: "File: Guide to advanced Git techniques.",
   },
   {
-    title: "Angular tour-of-heroes",
+    title: "Angular Tour of Heroes",
     link: "https://angular.io/tutorial/tour-of-heroes",
-    summary: "Step-by-step tutorial for Angular beginners.",
+    summary: "Doc: Step-by-step tutorial for Angular beginners.",
   },
   {
-    title: "Cloud Based Software",
+    title: "Cloud-Based Software",
     link: "https://moodle.bfh.ch/pluginfile.php/2312754/mod_resource/content/1/9_CloudbasierteSoftware.pdf",
-    summary: "Overview of cloud-based software solutions.",
+    summary: "Slides: Overview of cloud-based software solutions.",
   },
   {
     title: "REST, API & Microservice",
     link: "https://moodle.bfh.ch/pluginfile.php/2316141/mod_resource/content/1/WSEG%20%2310%20Microservice%20Architektur_Folien.pdf",
-    summary: "Introduction to REST, APIs, and microservices.",
+    summary: "Slides: Introduction to REST, APIs, and microservices.",
   },
   {
     title: "Closing Projects",
     link: "https://moodle.bfh.ch/pluginfile.php/2346634/mod_resource/content/1/12_SoftwareQualitaet.pdf",
-    summary: "Strategies for successfully closing projects.",
+    summary: "Slides: Strategies for successfully closing projects.",
   },
   {
     title: "Logic (Math)",
     link: "https://moodle.bfh.ch/pluginfile.php/1729487/mod_resource/content/1/WSEN_de_Lecture_02.pdf",
-    summary: "Mathematical logic concepts for software development.",
+    summary: "Slides: Mathematical logic concepts for software development.",
   },
   {
-    title: "IT doesn't matter",
+    title: "IT Doesn't Matter",
     link: "https://moodle.bfh.ch/pluginfile.php/1751571/mod_resource/content/7/A-ITDoesNotMatter%20Carr%20HBR%202003.pdf",
-    summary: "Discussion on the importance of IT in business.",
+    summary: "Article: Discussion on the importance of IT in business.",
   },
   {
     title: "Networks and Internet",
     link: "https://moodle.bfh.ch/pluginfile.php/1770969/mod_resource/content/2/WGWI%20Block%209%20-%20Netzwerke%2C%20Internet%2C%20HMTL_CSS.pdf",
-    summary: "Basics of networking and internet technologies.",
+    summary: "Slides: Basics of networking and internet technologies.",
   },
   {
     title: "Hofstede Framework",
     link: "https://moodle.bfh.ch/pluginfile.php/1906010/mod_resource/content/1/3_Cultural%20Frameworks_Hofstede.pdf",
-    summary: "Introduction to Hofstede's cultural dimensions theory.",
+    summary: "Slides: Introduction to Hofstede's cultural dimensions theory.",
   },
   {
     title: "SAP Implementation Case Study",
     link: "https://moodle.bfh.ch/pluginfile.php/1886191/mod_resource/content/1/Case%20study%20of%20SAP%20implementation%20in%20a%20corporation.pdf",
-    summary: "Case study on SAP implementation in a corporate setting.",
+    summary:
+      "Article: Case study on SAP implementation in a corporate setting.",
   },
   {
     title: "Big Data",
     link: "https://moodle.bfh.ch/pluginfile.php/1924506/mod_resource/content/1/CW17-big%20data.pdf",
-    summary: "Overview of big data concepts and technologies.",
+    summary: "Slides: Overview of big data concepts and technologies.",
   },
   {
     title: "Project Planning and SMART Goals",
     link: "https://moodle.bfh.ch/pluginfile.php/2073997/mod_resource/content/3/02_Planung%20Teil%201.pdf",
-    summary: "Techniques for project planning and setting SMART goals.",
+    summary: "Slides: Techniques for project planning and setting SMART goals.",
   },
   {
-    title: "Project Planning agil",
+    title: "Agile Project Planning",
     link: "https://moodle.bfh.ch/pluginfile.php/2107010/mod_resource/content/0/04a_Planung%20agil.pdf",
-    summary: "Agile project planning methodologies.",
+    summary: "Slides: Agile project planning methodologies.",
   },
   {
-    title: "Stakeholder- and Riskmanagement",
+    title: "Stakeholder and Risk Management",
     link: "https://moodle.bfh.ch/pluginfile.php/2083302/mod_resource/content/0/05_Stakeholder-%20und%20Risikomanagement.pdf",
-    summary: "Managing stakeholders and risks in projects.",
+    summary: "Slides: Managing stakeholders and risks in projects.",
   },
   {
     title: "Noun Project - Free Icons",
     link: "https://thenounproject.com/",
-    summary: "A collection of free icons for various uses.",
+    summary: "Website: A collection of free icons for various uses.",
   },
   {
     title: "Introduction to User Experience Design (video)",
     link: "https://learning.oreilly.com/videos/introduction-to-user/9780137534463/",
-    summary: "Video tutorial on user experience design principles.",
+    summary: "Video Link: Video tutorial on user experience design principles.",
   },
 ];
